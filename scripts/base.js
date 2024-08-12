c345fea51e401e186adc2449058120e3cfcc183b
Kevin Stares-Darbon <red.inshou@gmail.com>
2024-08-12 13:45:13 +0100

    inital code base push

--- scripts/base.js
+++ scripts/base.js
@@ -1,22 +1,101 @@
 // check linking
 
-console.log("base.js runs");
+console.dir("base.js runs");
 
-// import utilities
+// define utilities
 
-import { 
-    generateRandomNumber,
-    getPlayerGuess,
-    checkGuess
- } from "./utilities.js";
+const gameEngine = () => {
 
-//basic 1 turn wet run
+    // get and set functions
 
-let target_number = generateRandomNumber(1,100);
+    const getGameState = () => {
+        let _game_state = JSON.parse(JSON.stringify(game_state));
+        return _game_state;
+    }    
 
-let this_guess = await getPlayerGuess();
+    // initialisation
 
-console.log(this_guess, target_number);
+    const MAX_TURNS = 10;
+
+    const game_state = {
+        target_number: -1,
+        game_turn: 0,
+        prompting_for_guess: false,
+        current_guess: -1,
+        stored_guess: false,
+        guess_sequence: [],
+        game_won: false
+    }
+
+    // utility functions
+
+        
+    const generateRandomNumber = (min, max) => {
+        return (Math.floor(Math.random() * (max - min + 1)) + min);
+    }
 
 
-checkGuess(this_guess, target_number);
+    const checkGuess = (guess, target) => {
+        if (guess === target){
+            return "correct";
+        } else if (guess < target){
+            return "too low";
+        } else {
+            return "too high";
+        }
+    }
+
+    const getPlayerGuess = () => {
+        let newGuess = parseInt(prompt("Enter your Guess:"));
+        return newGuess;
+    }
+
+    const gameLoop = () => {
+        if (game_state.game_turn < MAX_TURNS){
+            game_state.prompting_for_guess = false;
+            game_state.stored_guess = false;
+
+            game_state.game_turn++;
+
+            console.log(`++++++++++ This is turn number ${game_state.game_turn} +++++++++++++++++`);
+
+            game_state.prompting_for_guess = true;
+            game_state.current_guess = getPlayerGuess();
+            game_state.guess_sequence[game_state.game_turn - 1] = game_state.current_guess;
+            game_state.stored_guess = true;
+
+            const check_string = checkGuess(game_state.current_guess, game_state.target_number);
+
+            if (check_string === "correct"){
+                console.log(`You did it!! The computer chose ${game_state.target_number} and on turn ${game_state.game_turn} you guessed ${game_state.current_guess}. Well done!!`);
+                game_state.game_won = true;
+            } else if (check_string === "too high"){
+                console.log(`Your guess is too high. You have ${MAX_TURNS - game_state.game_turn} tries left`);
+            } else if (check_string === "too low"){
+                console.log(`Your guess is too low. You have ${MAX_TURNS - game_state.game_turn} tries left`);
+            } else {
+                alert("Error in game logic");
+            }
+
+            if(!game_state.game_won){
+                if (game_state.game_turn < MAX_TURNS){
+                    gameLoop();
+                } else {
+                    console.log(`You are out of turns!! You failed to guess the correct number, which was ${game_state.target_number}`);
+                    console.log(`Better luck next time!!`);
+                }
+            } else {
+                console.log("Congratulations on winning the game!!");
+            }
+        }
+    }
+
+    game_state.target_number = generateRandomNumber(1, 100);
+
+    // run the recursing game loop
+    gameLoop();
+
+};
+
+// gameEngine replaces the basic 1 turn. Invoke the engine to play the game
+gameEngine();
